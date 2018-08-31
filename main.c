39f8ac3d494d07d95a79fcf4bd03a07944446603
Sergey Volkhin <chkrn@bk.ru>
2018-08-31 16:19:21 +0300

    Печатаем список файлов в директории.

--- main.c
+++ main.c
@@ -1,4 +1,29 @@
+// https://www.garron.me/en/go2linux/ls-file-permissions.html
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 int main(int argc, char *argv[])
 {
+  struct dirent *dp;
+  DIR *dirp = opendir(".");
+
+  if(dirp)
+  {
+    while(dp = readdir(dirp))
+    {
+      printf(">%s\n", dp->d_name);
+    }
+
+    if(closedir(dirp) != 0)
+      perror("Failed to close dir");
+  }
+  else
+  {
+    perror("Failed to open dir");
+    return -1;
+  }
+
   return 0;
 }
