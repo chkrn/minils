4e518c09a6c26a1d2281570289a3f1a575e0b416
Sergey Volkhin <chkrn@bk.ru>
2018-08-31 16:31:14 +0300

    Minils: Работает опция a.

--- main.c
+++ main.c
@@ -6,18 +6,41 @@
 
 int main(int argc, char *argv[])
 {
-  struct dirent *dp;
+  int flag_all = 0;
   DIR *dirp = opendir(".");
+
+  switch(argc)
+  {
+    case 1:
+    break; //< Ok, "ls -l" mode.
+
+    case 2:
+      if(argv[1][0] == 'a' && argv[1][1] == '\0') //< Ok, "ls -la" mode.
+      {
+        flag_all = 1;
+        break;
+      }
+
+    default:
+      fprintf(stderr, "Wrong options.\nUsage: %s [a]\n", argv[0]);
+      return -1;
+  }
 
   if(dirp)
   {
+    struct dirent *dp;
+
     while(dp = readdir(dirp))
     {
-      printf(">%s\n", dp->d_name);
+      if(flag_all || dp->d_name[0] != '.')
+        printf(">%s\n", dp->d_name);
     }
 
     if(closedir(dirp) != 0)
+    {
       perror("Failed to close dir");
+      return -1;
+    }
   }
   else
   {
