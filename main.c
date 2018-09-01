c5eb24129a2e1e6be272e6c376dece42c34a6a09
Sergey Volkhin <chkrn@bk.ru>
2018-09-01 16:16:14 +0300

    Minils: Пользователь может передать путь к файлу.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 static int is_visible(const struct dirent *d)
@@ -14,31 +15,43 @@ static int is_visible(const struct dirent *d)
 
 int main(int argc, char *argv[])
 {
-  int rval = -1;
-  int flag_all = 0;
-
   int i;
+  int rval = -1;
+
+  char *user_path = NULL;
+
   int files_num = 0;
   struct dirent **files = NULL;
+
+  int flag_all = 0; //< '-a' option.
+  if(argc > 1 && argv[1][0] == '-' && argv[1][1] == 'a' && argv[1][2] == '\0')
+    flag_all = 1;
 
   switch(argc)
   {
     case 1:
-    break; //< Ok, "ls -l" mode.
+    break;
 
     case 2:
-      if(argv[1][0] == 'a' && argv[1][1] == '\0') //< Ok, "ls -la" mode.
+      if(!flag_all)
+        user_path = argv[1];
+    break;
+
+    case 3:
+      if(flag_all)
       {
-        flag_all = 1;
+        user_path = argv[2];
         break;
       }
 
     default:
-      fprintf(stderr, "Wrong options.\nUsage: %s [a]\n", argv[0]);
+      fprintf(stderr, "Wrong options.\nUsage: %s [-a] [PATH]\n", argv[0]);
     goto fail;
   }
 
-  files_num = scandir(".", &files, flag_all ? NULL : is_visible, alphasort);
+  tzset(); // Init for localtime.
+
+  files_num = scandir(user_path ? user_path : ".", &files, flag_all ? NULL : is_visible, alphasort);
   if(files_num < 0)
   {
     files_num = 0;
@@ -46,15 +59,28 @@ int main(int argc, char *argv[])
     goto fail;
   }
 
-  tzset(); // Init for localtime.
-  
-
   for(i = 0; i < files_num; i++)
-    if(file_info_print(files[i]->d_name) != 0)
+  {
+    char *full_file_path = NULL;
+
+    if(user_path)
+    {
+      size_t full_file_path_len = strlen(user_path) + 1 /*delimeter*/ + strlen(files[i]->d_name);
+      full_file_path = malloc(full_file_path_len + 1 /*NULL-terminator*/);
+      snprintf(full_file_path, full_file_path_len + 1, "%s/%s", user_path, files[i]->d_name);
+    }
+    else
+      full_file_path = files[i]->d_name;
+
+    if(file_info_print(full_file_path) != 0)
     {
       perror("Failed to get file info");
       goto fail;
     }
+
+    if(user_path)
+      free(full_file_path);
+  }
 
   rval = 0;
 
