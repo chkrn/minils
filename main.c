dee3ebcc668537cf131d47f5ea12ddda23acdd44
Sergey Volkhin <chkrn@bk.ru>
2018-09-01 11:43:26 +0300

    Minils: Печатаем size и date.

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
-// https://www.garron.me/en/go2linux/ls-file-permissions.html
+// https://www.unix.com/man-page/posix/1posix/ls
 
 #include <dirent.h>
 #include <grp.h>
+#include <langinfo.h>
+#include <locale.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pwd.h>
+#include <time.h>
 #include <unistd.h>
 
-void print_id(const char* name, unsigned long int id)
+void print_id(const char* name, unsigned long long id)
 {
   if(name)
     printf(" %8s", name);
   else
-    printf(" %8lu", id);
+    printf(" %8llu", id);
 }
 
 int print_file_stat(const char *pathname)
@@ -33,6 +36,29 @@ int print_file_stat(const char *pathname)
     print_id(gr ? gr->gr_name : NULL, st.st_gid);
   } // GID <--
 
+  // File size.
+  printf(" %8llu", (unsigned long long)st.st_size);
+
+  { // Data and time -->
+    char str[256];
+    struct tm *tm = localtime(&st.st_mtime);
+
+    if(tm == NULL)
+    {
+      perror("Failed to get local time");
+      return -1;
+    }
+
+    if(strftime(str, sizeof(str), nl_langinfo(D_T_FMT), tm) == 0)
+    {
+      perror("Failed to format time string");
+      return -1;
+    }
+
+    printf(" %10s", str);
+  } // Data and time <--
+
+  // File name.
   printf(" %s\n", pathname);
 
   return 0;
@@ -59,6 +85,8 @@ int main(int argc, char *argv[])
       fprintf(stderr, "Wrong options.\nUsage: %s [a]\n", argv[0]);
     return -1;
   }
+
+  tzset(); // Init for localtime.
 
   if(dirp)
   {
